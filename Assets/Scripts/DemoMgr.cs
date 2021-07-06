3a80aa1feae79e1dff32cd34f668bf935561379e
Fareed Tahir <76517762+fareedTahir131@users.noreply.github.com>
2021-07-06 19:25:34 +0500

    project copied
    I just copied my project into clone repository.

---
+++ Assets/Scripts/DemoMgr.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AddressableAssets;
+
+public class DemoMgr : MonoBehaviour
+{
+    public AssetReference LocalNo;
+    // Start is called before the first frame update
+    void Start()
+    {
+        DisplayNo();
+    }
+
+    private void DisplayNo()
+    {
+        LocalNo.InstantiateAsync(Vector3.zero,Quaternion.identity);
+    }
+}
